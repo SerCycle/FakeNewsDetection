275b6d68ef2e55e157ede35664acb7f34ad6fa4d
SerCycle <michelyanuar@gmail.com>
2022-05-31 09:30:11 +0700

    bentar lagi kelar

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
-
 from flask import Flask, render_template, request, jsonify
-import flask
-import joblib
 import numpy as np
 from newsapi import NewsApiClient
 import requests
 import json
+import random
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import nltk
+import re
+import transformers
+from transformers import BertTokenizer
+from transformers import TFBertForSequenceClassification
+import tensorflow as tf
+from tensorflow import keras
+
+PRE_TRAINED_MODEL = 'indolem/indobert-base-uncased'
+bert_tokenizer = BertTokenizer.from_pretrained(PRE_TRAINED_MODEL)
+model = TFBertForSequenceClassification.from_pretrained(PRE_TRAINED_MODEL, num_labels = 2, from_pt=True)
+model.load_weights("model/nyoba_plis_dpt_89.h5")
 
 app = Flask(__name__)
 
@@ -18,10 +31,17 @@ def home():
     return render_template("index.html")
 
 @app.route("/hasil", methods=["POST"])
-def output():
-    text = request.form["text"]
-    # model = joblib.load('------')
-    return render_template('output.html', text=text)
+def hasil():
+    text = request.form["isiberita"]
+    input_text_tokenized =  bert_tokenizer.encode(text, truncation = True, padding = 'max_length', return_tensors = 'tf')
+    bert_predict = model(input_text_tokenized)
+    bert_output = tf.nn.softmax(bert_predict[0], axis = -1)
+    kategori = ['Valid', 'Hoax']
+    label = tf.argmax(bert_output, axis = 1)
+    label = label.numpy()
+    hasil = kategori[label[0]]
+
+    return render_template('output.html', text=hasil)
 
 @app.route("/about")
 def about():
